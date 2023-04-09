5529546665a1e9559798caa38ba3bfa317cc3344
prakriti0502 <prakriti1625.cse16@chitkara.edu.in>
2023-04-09 11:53:13 +0530

    first code

---
+++ ep-1/App.js
@@ -0,0 +1,21 @@
+
+const parent = React.createElement("div", {id: "parent"}, 
+    [
+        React.createElement("div", {id: "child"},
+            [
+                React.createElement("h1", {}, "I'm h1 tag"),
+                React.createElement("h2", {}, "I'm h2 tag")
+            ]
+        ),
+        React.createElement("div", {id: "child2"},
+            [
+                React.createElement("h1", {}, "I'm h1 tag of next child"),
+                React.createElement("h2", {}, "I'm h2 tag of next child")
+            ]
+        )
+    ]
+);
+// const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
