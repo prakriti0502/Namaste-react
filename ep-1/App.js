14b2f7981277c4c37fa34405bcfd0391c533cd8e
prakriti0502 <prakriti1625.cse16@chitkara.edu.in>
2023-04-19 15:14:07 +0530

    ep-3

--- ep-1/App.js
+++ ep-1/App.js
@@ -1,21 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id: "parent"}, 
-    [
-        React.createElement("div", {id: "child"},
-            [
-                React.createElement("h1", {}, "I'm h1 tag"),
-                React.createElement("h2", {}, "I'm h2 tag")
-            ]
-        ),
-        React.createElement("div", {id: "child2"},
-            [
-                React.createElement("h1", {}, "I'm h1 tag of next child"),
-                React.createElement("h2", {}, "I'm h2 tag of next child")
-            ]
-        )
-    ]
+const Title = () => (
+    <h2>Title of react</h2>
 );
-// const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
-console.log(parent);
+const num = 10;
+const HeadingComponent = () => (
+    <div>
+        <Title/>
+        <p>{num}</p>
+        <h1>Namaste js from functional component</h1>
+    </div>
+);
+
+const jsx = <h1>Hello from jsx</h1>
+console.log(jsx);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+
+root.render(<HeadingComponent/>);
+root2.render(<Title/>);
