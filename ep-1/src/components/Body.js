8881ce89b05eab18afa7ae6ecfed9f54daf320e7
prakriti0502 <prakriti1625.cse16@chitkara.edu.in>
2023-05-27 18:50:07 +0530

    useEffect hook

--- ep-1/src/components/Body.js
+++ ep-1/src/components/Body.js
@@ -1,15 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-    const [restaurants, setRestaurants] = useState(resList);
+    const [restaurants, setRestaurants] = useState([]);
+
+    useEffect(()=>fetchData(), []);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9720888&lng=77.694364&page_type=DESKTOP_WEB_LISTING"
+        );
+        const json = await data.json();
+        console.log(json);
+        setRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    }
+
+    if(restaurants.length === 0) {
+        return <Shimmer/>
+    }
+
     return (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={()=>
                 {
-                    setRestaurants(resList.filter(res=>res.data.avgRating>4));
+                    setRestaurants(restaurants.filter(res=>res.data.avgRating>4));
                     console.log(restaurants);
                 }}>Top Rated Restaurants</button>
             </div>
