44e04b8791c1f96834981da5c854a35590a64644
prakriti0502 <prakriti1625.cse16@chitkara.edu.in>
2023-06-20 19:45:48 +0530

    ep-8 pt-2 class components

--- ep-1/src/components/UserClass.js
+++ ep-1/src/components/UserClass.js
@@ -4,14 +4,23 @@ class UserClass extends React.Component {
 
     constructor(props) {
         super(props);
-        console.log(props)
+        // console.log(props);
+        this.state = {
+            //this contains all the state variables of component
+            count:0,
+            count2:2
+        };
     }
 
     render() {
+        const {name, location} = this.props;
+        const {count, count2} = this.state;
         return (
             <div className="user-card">
-                <h2>Name: {this.props.name}</h2>
-                <h3>Location: {this.props.location}</h3>
+                <h1>Count: {count}</h1>
+                <h1>Count2: {count2}</h1>
+                <h2>Name: {name}</h2>
+                <h3>Location: {location}</h3>
                 <h4>Contact: @pakkusakku</h4>
             </div>
         );
