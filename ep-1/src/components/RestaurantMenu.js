c092ec577da4449a6200f44328216d3270762aa8
prakriti0502 <prakriti1625.cse16@chitkara.edu.in>
2023-07-03 12:45:42 +0530

    ep-9 pt-1+2 Custom Hook

--- ep-1/src/components/RestaurantMenu.js
+++ ep-1/src/components/RestaurantMenu.js
@@ -1,29 +1,12 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
 
     const {resId} = useParams();
-    console.log(resId);
 
-    const [resInfo, setResInfo] = useState(null);
-
-    useEffect(()=>{
-        fetchMenu();
-    },[]);
-
-    const fetchMenu = async () => {
-        try {
-            const data = await fetch( MENU_API + resId);
-            const json = await data.json();
-            console.log("data from useeffect");
-            setResInfo(json.data);
-        } catch(error) {
-            console.log(error);
-        }
-    };
+    const resInfo = useRestaurantMenu(resId);
 
     if(resInfo===null) {
         return <Shimmer/>;
@@ -31,10 +14,7 @@ const RestaurantMenu = () => {
 
     const {name, cuisines, costForTwoMessage} = resInfo?.cards[0]?.card?.card?.info || {};
 
-    console.log("items assigning");
     const {itemCards} = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
-
-    console.log("items are ", resInfo?.cards[0]?.card?.card?.info);
 
     return (
         <div className="menu">
