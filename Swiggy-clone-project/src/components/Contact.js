8e185972dfc7f542b46f15b57483fabd45e857a2
prakriti0502 <prakriti1625.cse16@chitkara.edu.in>
2023-09-08 19:08:25 +0530

    Unit Testing in react

--- Swiggy-clone-project/src/components/Contact.js
+++ Swiggy-clone-project/src/components/Contact.js
@@ -1,7 +1,12 @@
 const Contact = () => {
     return (
         <div>
-            <h1>Contact Us</h1>
+            <h1 className="p-4 m-4 text-3xl">Contact Us</h1>
+            <form>
+                <input className="border border-black p-2 m-2" placeholder="name" type="text"/>
+                <input className="border border-black p-2 m-2" placeholder="query" type="text"/>
+                <button className="border border-black p-2 m-2 bg-gray-200">Submit</button>
+            </form>
         </div>
     );
 }
---
+++ Swiggy-clone-project/src/components/__tests__/Contact.test.js
@@ -0,0 +1,27 @@
+import {render, screen} from "@testing-library/react";
+import Contact from '../Contact';
+import '@testing-library/jest-dom';
+
+test('should load contact component', () => { 
+    render(<Contact/>);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+});
+
+test('should render a button in contact component', () => {
+    render(<Contact/>);
+    const button = screen.getByText("Submit");
+    expect(button).toBeInTheDocument();
+});
+
+test('should render 2 input boxes', () => {
+    render(<Contact/>);
+    const inputs = screen.getAllByRole("textbox");
+    expect(inputs.length).toBe(2);
+});
+
+test('Should render input with name placeholder', () => {
+    render(<Contact/>);
+    const inputName = screen.getByPlaceholderText("name");
+    expect(inputName).toBeInTheDocument();
+});
